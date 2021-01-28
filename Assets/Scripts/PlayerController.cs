1ba693aafaae46c9f34d61b941afc3a6ea2b2214
Ron Welch <mmcelt@yahoo.com>
2021-01-28 16:26:00 -0700

    8 - 4 Direction Movement Animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,10 @@ public class PlayerController : MonoBehaviour
 	#region Fields & Properties
 
 	[SerializeField] float _moveSpeed;
-	[SerializeField] Rigidbody2D _theRB;
+	[SerializeField] SpriteRenderer _theSR;
+	[SerializeField] Sprite[] _playerDirectionSprites;
 
+	Rigidbody2D _theRB;
 	Animator _theAnim;
 
 	#endregion
@@ -22,6 +24,7 @@ public class PlayerController : MonoBehaviour
 
 	void Start() 
 	{
+		_theRB = GetComponent<Rigidbody2D>();
 		_theAnim = GetComponent<Animator>();
 	}
 	
@@ -32,6 +35,34 @@ public class PlayerController : MonoBehaviour
 		_theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * _moveSpeed;
 
 		_theAnim.SetFloat("Speed", _theRB.velocity.magnitude);
+
+		if (_theRB.velocity != Vector2.zero)
+		{
+			if (Input.GetAxisRaw("Horizontal") != 0)
+			{
+				_theSR.sprite = _playerDirectionSprites[1];
+
+				if (Input.GetAxisRaw("Horizontal") < 0)
+				{
+					_theSR.flipX = true;
+				}
+				else
+				{
+					_theSR.flipX = false;
+				}
+			}
+			else
+			{
+				if (Input.GetAxisRaw("Vertical") < 0)
+				{
+					_theSR.sprite = _playerDirectionSprites[0];
+				}
+				else
+				{
+					_theSR.sprite = _playerDirectionSprites[2];
+				}
+			}
+		}
 	}
 	#endregion
 
