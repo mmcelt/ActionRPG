e7f70ad2bd2c468fbcec884bfeb1543aa36e00e6
Ron Welch <mmcelt@yahoo.com>
2021-01-29 11:52:19 -0700

    12 - Limiting the Camera Movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,7 +6,12 @@ public class CameraController : MonoBehaviour
 {
 	#region Fields & Properties
 
-	[SerializeField] Transform _target;
+	[SerializeField] BoxCollider2D _areaBox;
+
+	Transform _target;
+	Camera _theCam;
+
+	float _halfWidth, _halfHeight;
 
 	#endregion
 
@@ -19,12 +24,21 @@ public class CameraController : MonoBehaviour
 
 	void Start() 
 	{
+		_theCam = GetComponent<Camera>();
 		_target = PlayerController.Instance.transform;
+
+		_halfHeight = _theCam.orthographicSize;
+		_halfWidth = _theCam.orthographicSize * _theCam.aspect;
 	}
 	
 	void LateUpdate() 
 	{
 		transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y, transform.position.z);
+
+		transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x, _areaBox.bounds.min.x + _halfWidth, _areaBox.bounds.max.x - _halfWidth), 
+			Mathf.Clamp(transform.position.y, _areaBox.bounds.min.y + _halfHeight, _areaBox.bounds.max.y - _halfHeight), 
+			transform.position.z);
 	}
 	#endregion
 
