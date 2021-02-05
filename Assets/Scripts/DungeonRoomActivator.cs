64336c9a0f5da7deb8aae6e3afc2372e40976600
Ron Welch <mmcelt@yahoo.com>
2021-02-04 18:22:04 -0700

    51 - Sealing & Opening Rooms

--- Assets/Scripts/DungeonRoomActivator.cs
+++ Assets/Scripts/DungeonRoomActivator.cs
@@ -10,6 +10,11 @@ public class DungeonRoomActivator : MonoBehaviour
 
 	List<GameObject> _clonedEnemies;
 
+	[SerializeField] bool _lockDoors;
+	[SerializeField] GameObject[] _doors;
+
+	bool _doorsLocked, _dontRespawnEnemies;
+
 	#endregion
 
 	#region Getters
@@ -28,12 +33,41 @@ public class DungeonRoomActivator : MonoBehaviour
 
 	}
 
+	void Update()
+	{
+		if (_doorsLocked)
+		{
+			bool enemyFound = false;
+			//DIFFERENT FROM COURSE CODE DUE TO THE FACT I REMOVE DEAD ENEMIES FROM THE
+			//CLONEDPLAYERS LIST AS THEY GET KILLED...
+			if (_clonedEnemies.Count > 0) enemyFound = true;
+
+			if (!enemyFound)
+			{
+				foreach (GameObject door in _doors)
+					door.SetActive(false);
+
+				_doorsLocked = false;
+				_lockDoors = false;
+				_dontRespawnEnemies = true;
+			}
+		}
+	}
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.CompareTag("Player"))
 		{
 			DungeonCameraController.Instance._targetPoint = new Vector3(transform.position.x, transform.position.y, DungeonCameraController.Instance._targetPoint.z);
 			SpawnEnemies();
+
+			if (_lockDoors)
+			{
+				foreach (GameObject door in _doors)
+					door.SetActive(true);
+
+				_doorsLocked = true;
+			}
 		}
 	}
 
@@ -60,6 +94,8 @@ public class DungeonRoomActivator : MonoBehaviour
 
 	void SpawnEnemies()
 	{
+		if (_dontRespawnEnemies) return;
+
 		foreach (GameObject enemy in _allEnemies)
 		{
 			GameObject newEnemy = Instantiate(enemy, enemy.transform.position, Quaternion.identity);
--- Assets/Scripts/Enemies/EnemyHealthController.cs
+++ Assets/Scripts/Enemies/EnemyHealthController.cs
@@ -47,10 +47,11 @@ public class EnemyHealthController : MonoBehaviour
 		if (_currentHealth <= 0)
 		{
 			Instantiate(_deathEffect, transform.position, Quaternion.identity);
-			//if (_areaActivator != null)
-			//	_areaActivator.RemoveEnemy(gameObject);	//remove enemy from area List
-			//else
-			//	_dungeonAreaActivator.RemoveEnemy(gameObject);
+
+			if (_areaActivator != null)
+				_areaActivator.RemoveEnemy(gameObject);   //remove enemy from area List
+			else
+				_dungeonAreaActivator.RemoveEnemy(gameObject);  //remove enemy from dungeon List
 
 			AudioManager.Instance.PlaySFX(4);
 			Destroy(gameObject);
