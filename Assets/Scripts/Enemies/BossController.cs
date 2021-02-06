ca2bf0e0458499d1aa61972a496b3ec754020349
Ron Welch <mmcelt@yahoo.com>
2021-02-06 13:57:05 -0700

    58 - Making the Boss Move

---
+++ Assets/Scripts/Enemies/BossController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+	#region Fields & Properties
+
+	[SerializeField] GameObject _theBossSprite, _door;
+	[SerializeField] Transform[] _spawnPoints;
+	[SerializeField] float _moveSpeed, _timeActive, _timeBetweenSpawns, _initalSpawnDelay;
+
+	Vector3 _moveTarget;
+	float _activeCounter, _spawnCounter;
+
+	#endregion
+
+	#region Getters
+
+
+	#endregion
+
+	#region Unity Methods
+
+	void Start() 
+	{
+		_door.SetActive(true);
+		_spawnCounter = _initalSpawnDelay;
+	}
+	
+	void Update() 
+	{
+		if (_spawnCounter > 0)
+		{
+			_spawnCounter -= Time.deltaTime;
+
+			if (_spawnCounter <= 0)
+			{
+				_activeCounter = _timeActive;
+
+				_theBossSprite.transform.position = _spawnPoints[Random.Range(0, _spawnPoints.Length)].position;
+
+				_moveTarget = _spawnPoints[Random.Range(0, _spawnPoints.Length)].position;
+
+				while (_moveTarget == _theBossSprite.transform.position)
+				{
+					_moveTarget = _spawnPoints[Random.Range(0, _spawnPoints.Length)].position;
+				}
+
+				_theBossSprite.SetActive(true);
+			}
+		}
+		else
+		{
+			_activeCounter -= Time.deltaTime;
+
+			if (_activeCounter <= 0)
+			{
+				_spawnCounter = _timeBetweenSpawns;
+				_theBossSprite.SetActive(false);
+			}
+
+			_theBossSprite.transform.position = Vector3.MoveTowards(_theBossSprite.transform.position, _moveTarget, _moveSpeed * Time.deltaTime);
+		}
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
