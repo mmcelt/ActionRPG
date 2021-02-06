194d80ab2a0986e8ce17bb948deaf535b1f90eeb
Ron Welch <mmcelt@yahoo.com>
2021-02-06 16:58:54 -0700

    61 - Making the Boss Shoot

--- Assets/Scripts/Enemies/BossController.cs
+++ Assets/Scripts/Enemies/BossController.cs
@@ -16,6 +16,15 @@ public class BossController : MonoBehaviour
 	Vector3 _moveTarget;
 	float _activeCounter, _spawnCounter;
 
+	[Header("Shooting")]
+	[SerializeField] BossBullet _bossBulletPrefab;
+	[SerializeField] Transform[] _firePoints;
+	[SerializeField] Transform _fireCenter;
+	[SerializeField] float _timeBetweenShots, _shotRotateSpeed, _shootTime;
+	[SerializeField] int _stage2Threshold, _stage3Threshold;
+
+	float _shotCounter, _shootingCounter;
+
 	#endregion
 
 	#region Getters
@@ -25,13 +34,13 @@ public class BossController : MonoBehaviour
 
 	#region Unity Methods
 
-	void Start() 
+	void Start()
 	{
 		_door.SetActive(true);
 		_spawnCounter = _initalSpawnDelay;
 	}
-	
-	void Update() 
+
+	void Update()
 	{
 		if (_bossHealth <= 0) return;
 
@@ -42,6 +51,7 @@ public class BossController : MonoBehaviour
 			if (_spawnCounter <= 0)
 			{
 				_activeCounter = _timeActive;
+				_shootingCounter = _shootTime;
 
 				_theBossSprite.transform.position = _spawnPoints[Random.Range(0, _spawnPoints.Length)].position;
 
@@ -66,6 +76,38 @@ public class BossController : MonoBehaviour
 			}
 
 			_theBossSprite.transform.position = Vector3.MoveTowards(_theBossSprite.transform.position, _moveTarget, _moveSpeed * Time.deltaTime);
+			//shooting...
+			if (_shootingCounter > 0)
+			{
+				_shootingCounter -= Time.deltaTime;
+
+				_shotCounter -= Time.deltaTime;
+
+				if (_shotCounter <= 0)
+				{
+					_shotCounter = _timeBetweenShots;
+
+					if (_bossHealth > _stage2Threshold)
+					{
+						for (int i = 0; i < 4; i++)
+						{
+							Instantiate(_bossBulletPrefab, _firePoints[i].position, _firePoints[i].rotation).SetDirection(_fireCenter.position);
+						}
+					}
+					else
+					{
+						for (int i = 0; i < _firePoints.Length; i++)
+						{
+							Instantiate(_bossBulletPrefab, _firePoints[i].position, _firePoints[i].rotation).SetDirection(_fireCenter.position);
+						}
+					}
+				}
+
+				if (_bossHealth <= _stage3Threshold)
+				{
+					_fireCenter.transform.rotation = Quaternion.Euler(_fireCenter.transform.rotation.eulerAngles.x, _fireCenter.transform.rotation.eulerAngles.y, _fireCenter.transform.rotation.eulerAngles.z + (_shotRotateSpeed * Time.deltaTime));
+				}
+			}
 		}
 	}
 	#endregion
